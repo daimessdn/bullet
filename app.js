59307da4c622e56fdedad3a606db5fe56817bde0
daimessdn <daimessdn@gmail.com>
2020-07-17 15:16:31 +0700

    init'd config for ejs and also index.ejs

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
-# start the server
-app.listen(() => {
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+// routing
+app.get("/", (request, response) => {
+	response.render("index.ejs");
+});
+
+// start the server
+app.listen(3000, () => {
 	console.log("listening into localhost:3000");
 });
